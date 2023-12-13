c064638013e0796b30e1879e40214fd707dfbbd3
Natneam M <se.natneam.mesele@gmail.com>
2023-12-13 07:13:57 +0300

    Add file upload endpoint

--- api/app.py
+++ api/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, jsonify, make_response
+import os
+from flask import Flask, request, jsonify, abort
+
+UPLOAD_DIRECTORY = "./data"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
 
 app = Flask(__name__)
 
 
-@app.route('/api/v1.0/test', methods=['GET'])
-def test_response():
-    """Return a sample JSON response."""
-    sample_response = {
-        "items": [
-            { "id": 1, "name": "Apples",  "price": "$2" },
-            { "id": 2, "name": "Peaches", "price": "$5" }
-        ]
-    }
-    response = make_response(jsonify(sample_response))
+#file upload endpoint
+@app.route('/upload/<filename>', methods=['POST'])
+def upload(filename):
+    if "/" in filename:
+        abort(400, "no subdirectories allowed")
 
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-    
-    return response
+    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
+        fp.write(request.files['file'].read())
+
+    return jsonify({"message" : "File successfully uploaded"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
